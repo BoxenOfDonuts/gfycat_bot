fa0dbe8f0f0f369c4604dfa500950226f630d76f
Joel <joel.hacke@gmail.com>
2019-02-28 14:23:29 -0600

    unique uuid per run

--- logger.py
+++ logger.py
@@ -1,9 +1,11 @@
 import logging
+import uuid
 from pythonjsonlogger import jsonlogger
 
+extra={'runID': str(uuid.uuid4())}
 LOGFILE = '/mnt/gfycat_bot/gfycat_bot.log'
 logHandler = logging.FileHandler(filename=LOGFILE)
-formatter = jsonlogger.JsonFormatter('%(asctime)s %(message)s')
+formatter = jsonlogger.JsonFormatter('%(asctime)s %(runID)s %(message)s')
 logHandler.setFormatter(formatter)
 logging.getLogger().addHandler(logHandler)
 logging.getLogger().setLevel(logging.INFO)
